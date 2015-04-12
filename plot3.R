79e1d9f4e5654d43e908562b242bd865d15b2bfb
Daniel Marjenburgh <dmarjenburgh@gmail.com>
2015-04-12 13:44:41 +0200

    fix typo in plot3 code and make simplifications

--- plot3.R
+++ plot3.R
@@ -5,26 +5,27 @@ fullDataset <- read.csv(fileName, sep=";", na.strings=c("","?"))
 dataset <- subset(fullDataset, Date %in% c("1/2/2007", "2/2/2007"))
 
 ## Parse datetimes
-datetimes <- paste(dataset$Date, dataset$Time)
-dataset$DateTime = strptime(datetimes, "%d/%m/%Y %H:%M:%S")
+datetime <- strptime(paste(dataset$Date, dataset$Time), "%d/%m/%Y %H:%M:%S")
+
+## Get sub metering data
+subMetering <- dataset[7:9]
 
 ## Plot3: Energy sub metering
 png(filename = "plot3.png", width = 480, height = 480)
 
-plot(dataset$DateTime,
-     dataset$Subset_metering_1,
+plot(datetime,
+     subMetering[[1]],
      type = "n",
      ylab = "Energy sub metering",
      xlab = "")
 
 # Add 3 sub metering categories separately with different colors
 plotColors <- c("black", "red", "blue")
-points(dataset$DateTime, dataset$Sub_metering_1, type = "l", col = plotColors[1])
-points(dataset$DateTime, dataset$Sub_metering_2, type = "l", col = plotColors[2])
-points(dataset$DateTime, dataset$Sub_metering_3, type = "l", col = plotColors[3])
+points(datetime, subMetering[[1]], type = "l", col = plotColors[1])
+points(datetime, subMetering[[2]], type = "l", col = plotColors[2])
+points(datetime, subMetering[[3]], type = "l", col = plotColors[3])
 
 # Add legend
-legend("topright", names(dataset[7:9]), lty = 1, col = plotColors)
+legend("topright", names(subMetering), lty = 1, col = plotColors)
 
 dev.off()
-
